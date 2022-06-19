9e78f6cd8b2b56e78f82f9d556cdf141c7eacb22
facus <facus@facus.com>
2022-06-18 21:49:22 -0300

    Prueba 1 de cursos

--- course/models.py
+++ course/models.py
@@ -1,11 +1,21 @@
+from cgi import print_exception
 from django.db import models
 
 
 class Course(models.Model):
+    TYPES = (
+        ('AB', 'AB'),
+        ('CO', 'CO'),
+        ('GE', 'GE'),
+    )
     name = models.CharField(max_length=40)
-    code = models.IntegerField()
     description = models.TextField(blank = True, null=True)
+    owner = models.CharField(max_length=40)
+    teacher = models.CharField(max_length=40)
+    price = models.IntegerField
+    type = models.CharField(max_length=2, choices=TYPES)
+    date_start = models.DateField
 
 
     def __str__(self):
-        return f'{self.name} course --'
+        return f'{self.name}'
